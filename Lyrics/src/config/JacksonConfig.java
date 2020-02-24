8eef0f2e0750e1861211e7afdcd623dd8b2c269c
FlorianKovacic <kovacsics.florian@yahoo.com>
2020-02-24 19:22:20 +0100

    edited config

--- Lyrics/src/config/JacksonConfig.java
+++ Lyrics/src/config/JacksonConfig.java
@@ -7,6 +7,7 @@ import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module.Feature;
 
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
@@ -15,7 +16,10 @@ public class JacksonConfig implements ContextResolver<ObjectMapper> {
 	@Override
 	public ObjectMapper getContext(Class<?> type) {
 		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(new Hibernate5Module());
+		Hibernate5Module h5m = new Hibernate5Module();
+		h5m.disable(Feature.FORCE_LAZY_LOADING);
+		h5m.enable(Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
+		mapper.registerModule(h5m);
 		return mapper;
 	}
 
